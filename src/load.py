934b377c9b22f4c3f57e98270e56d1fde99182d3
Aditya Prakash <adityaprakash@users.noreply.github.com>
2025-12-10 23:58:16 +0530

    path changes

--- src/load.py
+++ src/load.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def load_to_db(df, table="meteorites"):
-    conn = sqlite3.connect("etl.db")
+    conn = sqlite3.connect("data/processed/meteorites.db")
     df.to_sql(table, conn, if_exists="replace", index=False)
     conn.close()
     print("Loaded to database:", table)
